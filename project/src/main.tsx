b993fea2940aaf70c7675f04eead35a79e970880
Chirag Vishwakarma <vishwakarmachirag293@gmail.com>
2025-03-01 15:20:23 +0530

    Update main.tsx

--- project/src/main.tsx
+++ project/src/main.tsx
@@ -3,6 +3,10 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
+// Initialize EmailJS (you'll need to replace this with your actual public key)
+// import emailjs from '@emailjs/browser';
+// emailjs.init("YOUR_PUBLIC_KEY");
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
