136228dcb819c5cea7c1ba10b53d3822e8fa3daf
Chirag230861 <vishwakarmachirag293@gmail.com>
2025-03-01 11:05:15 +0530

    Update App.tsx

--- project/src/App.tsx
+++ project/src/App.tsx
@@ -192,6 +192,13 @@ const projects: Project[] = [
     imageUrl: "https://i.postimg.cc/vZQH74KW/DALL-E-2025-01-24-15-23-03-A-conceptual-illustration-of-an-eye-tracker-mouse-system-The-scene-fea.webp",
     githubUrl: "https://github.com/Chirag230861/EyeTracker",
   },
+   {
+    title: "",
+    description: "",
+    technologies: ["Python:pyautogui:MediaPipe", "NLP"],
+    imageUrl: "",
+    githubUrl: "",
+  },
 ];
 
 const contact: ContactType = {
